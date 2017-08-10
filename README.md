36b5d1bb6e9e588973fbdaac586fb4c2b289e6c9
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-10 18:30:10 +1200

    initial

---
+++ README.md
@@ -0,0 +1,63 @@
+# depject-tree
+
+tool for showing all dependencies in a depject project (or projects)
+
+## usage
+
+```
+> npm install -g depject-tree
+
+> depject-tree . node_modules/patchcore
+... outputs json object...
+```
+the format is:
+```
+<path>: {
+  "needs": {
+    <api>: [<places this api is provided>],
+  },
+  "gives": {
+    <api>: [<places this api is used>]
+  }
+}
+```
+
+here is a portion of the output for patchbay@6
+it represents the _split_ module, which shows two or more
+`screen_view`s side by side. it also _gives_ `screen_view`
+which is used by the `app`, `tabs`, and `split` modules.
+```
+...
+"modules_extra/split.js": {
+  "needs": {
+    "screen_view": [
+      "modules_basic/invite.js",
+      "modules_basic/setup.js",
+      "modules_extra/blob.js",
+      "modules_extra/key.js",
+      "modules_extra/network.js",
+      "modules_extra/query.js",
+      "modules_extra/versions.js"
+    ]
+  },
+  "gives": {
+    "screen_view": [
+      "modules_core/app.js",
+      "modules_core/tabs.js",
+      "modules_extra/split.js"
+    ]
+  }
+},
+...
+```
+
+
+## todo
+
+* prettier output
+* make it easy to quickly query about specific modules
+
+## License
+
+MIT
+
---
+++ index.js
@@ -0,0 +1,92 @@
+var fs = require('fs')
+var path = require('path')
+var isDepject = require('depject/is')
+
+function Filter (stat) {
+  if(stat.name === 'node_modules') return
+  if(/^\./.test(stat.name)) return
+  if(stat.isDirectory()) return true
+  if(/\.js$/.test(stat.name)) return true
+}
+
+function Map (stat) {
+  try {
+    var m = require(stat.path)
+    if(isDepject(m))
+      return m
+  } catch (err) {
+    return err.message
+  }
+}
+
+function walk (dir, filter, map) {
+  filter = filter || Filter
+  map = map || Map
+  var o = {}
+  fs.readdirSync(dir)
+  .forEach(function (file) {
+    var filename = path.join(dir, file)
+    var stat = fs.statSync(filename)
+    stat.name = file
+    stat.path = filename
+
+    if(!filter(stat)) return
+    if(stat.isDirectory())
+      o[file] = walk(filename, filter, map)
+    else
+      o[file] = map(stat)
+  })
+  return o
+}
+
+var files = {}
+var dirs = process.argv.slice(2)
+if(!dirs.length) dirs = [process.cwd()]
+
+dirs.forEach(function (dir) {
+  walk(
+    path.resolve(process.cwd(), dir),
+    Filter,
+    function (e) {
+      files[e.path] = Map(e)
+    }
+  )
+})
+var _files = files; files = {}
+for(var k in _files)
+  files[path.relative(process.cwd(), k)] = _files[k]
+
+var N = require('libnested')
+
+var files = N.map(files, function (value, path) {
+  var last = path[path.length - 1]
+  if(path[1] == 'gives') {
+    if('string' === typeof value)
+      path.push(value)
+//    return value
+    var needed = []
+    for(var k in files) {
+      var m = files[k]
+      if(m && m.needs && N.get(m.needs, path.slice(2)))
+        needed.push(k)
+    }
+    return needed
+  }
+  else if(path[1] == 'needs') {
+    var given = []
+    for(var k in files) {
+      var m = files[k]
+      if(m && m.gives && N.get(m.gives, path.slice(2)))
+        given.push(k)
+    }
+    return given
+  }
+
+})
+
+console.log(JSON.stringify(files, null, 2))
+process.exit(0)
+
+
+
+
