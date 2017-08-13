8757fa1f5b9daab68e5a1fd3e8410cdd4f780f76
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-13 17:38:14 +1200

    fix graph.js

--- graph.js
+++ graph.js
@@ -1,30 +1,5 @@
 
 var N = require('libnested')
-module.exports = function (deps) {
-  var files = {}
-  var apis = {}
-  for(var k in deps) {
-    if(deps[k] && deps[k].gives)
-      delete deps[k].gives
-  }
-  N.each(deps, function (v, path) {
-    var file = path[0]
-    var api = path.slice(2).join('.')
-    if(api)
-      ;(files[file] = files[file] || []).push(api)
-
-    if(file)
-      ;(apis[api] = apis[api] || []).push(file)
-    
-  })
-
-  return toDot(apis, files)
-}
-
-function dot (s) {
-  return JSON.stringify(s)
-  return s.replace('.js', '_JS').replace(/-/g, '_').replace(/\.|\//g, '_')
-}
 
 function toDot(a, b, name) {
   name = name || ''
@@ -36,16 +11,117 @@ function toDot(a, b, name) {
 
   log('digraph ' + (name || 'depject') + ' {')
 
-  for(var k in a) log('  ', dot(k), '[label='+dot(k)+']')
+  var added = {}
+  function add (string) {
+    if(!added[string]) {
+      added[string] = true
+      log(string)
+    }
+  }
+
   for(var k in b) log('  ', dot(k), '[label='+dot(k)+', shape="box"]')
+  for(var k in a) if(k) log('  ', dot(k), '[label='+dot(k)+']')
+
   for(var k in a)
     for(var i = 0; i < a[k].length; i++)
       if(k && a[k][i])
-        log(dot(k)+'->'+dot(a[k][i]))
+        add(dot(k)+'->'+dot(a[k][i]))
+
   for(var k in b)
     for(var i = 0; i < b[k].length; i++)
       if(k && b[k][i])
-        log(dot(k)+'->'+dot(b[k][i]))
+        add(dot(k)+'->'+dot(b[k][i]))
+
+  log('}')
+
+  return s
+}
+
+function dot (s) {
+  return JSON.stringify(s)
+}
+
+function toGraph(deps) {
+  var files = {}
+  var apis = {}
+  for(var k in deps) {
+    if(deps[k] && deps[k].gives) {
+      deps[k] = deps[k].gives
+    }
+  }
+  N.each(deps, function (v, path) {
+    var file = path[0]
+    var api = path.slice(1).join('.')
+    if(api)
+      ;(files[file] = files[file] || []).push(api)
+
+    if(v) v.forEach(function (_file) {
+      ;(apis[api] = apis[api] || []).push(_file)
+    })
+  })
+
+  return toDot(apis, files)
+}
+
+function toLabeledGraph (deps, name) {
+  var s = ''
+  function log () {
+    var args = [].slice.call(arguments)
+    s += args.join(' ') + '\n'
+  }
+
+  var graph = {}
+  var nodes = {}
+  for(var k in deps) {
+    if(deps[k] && deps[k].gives) {
+      deps[k] = deps[k].gives
+    }
+  }
+  N.each(deps, function (v, path) {
+    var file = path[0]
+    var api = path.slice(1).join('.')
+    if(v) {
+      v.forEach(function (used) {
+        nodes[file] = true
+        nodes[used] = true
+        nodes[api] = false
+        N.set(graph, [file, api, used], true)
+      })
+    }
+  })
+
+  console.error(JSON.stringify(graph, null, 2))
+
+  var added = {}
+  function add (string) {
+    if(!added[string]) {
+      added[string] = true
+      log(string)
+    }
+  }
+
+  log('digraph ' + (name || 'depject') + ' {')
+
+  for(var k in nodes) {
+    if(nodes[k])
+      log('  ', dot(k), '[label='+dot(k)+', shape="box"]')
+    else
+      log('  ', dot(k), '[label='+dot(k)+']') //apis
+  }
+
+  for(var k in graph) {
+    for(var j in graph[k]) {
+      add(dot(k)+'->'+dot(j))
+    }
+  }
+
+  for(var k in graph) {
+    for(var j in graph[k]) {
+      for(var l in graph[k][j])
+        add(dot(j)+'->'+dot(l))
+    }
+  }
+
 
   log('}')
 
@@ -58,7 +134,8 @@ if(!module.parent) {
   var log = console.log
 
   console.log = function () {}
-  log(module.exports(require('./')(dirs)))
+  log(toLabeledGraph(require('./')(dirs)))
+//  log(module.exports(require('./')(dirs)))
   process.exit(0)
 }
 
